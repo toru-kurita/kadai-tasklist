14b506dc619b99f4ef3e36e4f6af117db8bd5b18
Toru_Kurita <kamome1101@gmail.com>
2017-09-03 02:04:17 +0000

    add_stasus(Status cant be blank)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,10 +1,12 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  
+  
   def index
     @tasks = Task.all 
   end
 
   def show
-    @task = Task.find(params[:id])
   end
   
   def new
@@ -24,12 +26,10 @@ class TasksController < ApplicationController
   end  
   
   def edit
-    @task = Task.find(params[:id])
   end
   
   def update
-    @task = Task.find(params[:id])
-    
+
     if @task.update(task_params)
       flash[:success] = "Taskが正常に更新されました"
       redirect_to @task
@@ -40,7 +40,6 @@ class TasksController < ApplicationController
   end
   
   def destroy
-    @task = Task.find(params[:id])
     @task.destroy
 
     flash[:success] = "Taskが削除されました"
@@ -48,6 +47,10 @@ class TasksController < ApplicationController
   end
   
 private
+
+def set_task
+  @task = Task.find(params[:id])
+end
 
 # Strong Parameter
 def task_params
