3d4b2b296cc0c714da5326d7ca0b4975b411f5e7
Toru_Kurita <kamome1101@gmail.com>
2017-09-16 12:52:24 +0000

    user_session

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,9 +1,14 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
-  
-  
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
+
+
   def index
-    @tasks = Task.all 
+    if logged_in?
+      @user = current_user
+      @tasks = current_user.tasks.page(params[:page])
+    end
   end
 
   def show
@@ -14,10 +19,9 @@ class TasksController < ApplicationController
   end
   
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Taskが正常に登録されました'
+      flash[:success] = 'Taskが登録されました'
       redirect_to @task
     else
       flash.now[:danger] = 'Taskが登録できませんでした'
@@ -41,20 +45,26 @@ class TasksController < ApplicationController
   
   def destroy
     @task.destroy
-
     flash[:success] = "Taskが削除されました"
     redirect_to tasks_url
   end
   
-private
+  private
 
-def set_task
-  @task = Task.find(params[:id])
-end
+  def set_task
+    @task = Task.find(params[:id])
+  end
 
-# Strong Parameter
-def task_params
-  params.require(:task).permit(:content, :status)
-end
+  # Strong Parameter
+  def task_params
+    params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end
 
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+  before_action :require_user_logged_in, only: [:show]
+
+  def show
+    if logged_in?
+      @user = current_user
+    end  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    
+    if @user.save
+      flash[:success] = 'アカウントの登録が完了しました'
+      redirect_to tasks_url
+    else
+      flash.now[:danger] = 'アカウントの登録に失敗しました'
+      render :new
+    end
+  end
+  
+  private
+  
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
